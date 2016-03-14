d6807bac27b80ca6c92eab3346859070520f8219
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2016-03-14 17:28:10 +1300

    unnecessary code removed

--- Spiking-Neural-Network/Spike-Encoding/Utility/JSONParser.py
+++ Spiking-Neural-Network/Spike-Encoding/Utility/JSONParser.py
@@ -1,13 +1,9 @@
-import os
 import json
 
 
 class JSONParser:
     def __init__(self, location=None):
-        if location is None:
-            self.current_folder = os.path.dirname(os.path.realpath(__file__)) + "/data/"
-        else:
-            self.current_folder = location
+        self.current_folder = location
 
     def get_json(self):
 
